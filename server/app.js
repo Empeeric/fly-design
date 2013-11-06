60710039b4cf7ef50a167228106ff874ab48c1fb
eyy <etaypere@gmail.com>
2013-11-06 18:02:01 +0200

    node

---
+++ server/app.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+var app = express();
+
+app.set('site', 'Fly Design');
+app.set('port', process.env.PORT || 80);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
