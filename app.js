5ca25fb931acd21658925f219ebf4f5f21a7926a
eyy <etaypere@gmail.com>
2013-11-11 20:47:25 +0200

    /third start

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.get('/', function(req, res) {
 app.get('/second', function(req, res) {
     res.render('second');
 });
+app.get('/third', function(req, res) {
+    res.render('third');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
