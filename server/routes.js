24f865e09acac80855cabef9745dcf715deb374d
Refael Ackermann <refael@empeeric.com>
2014-03-31 21:33:14 +0300

    mail thank you

--- server/routes.js
+++ server/routes.js
@@ -6,44 +6,47 @@ var registry = require('./global'),
         auth: registry.SENDGRID_AUTH
     }),
     dust = require('dustjs-linkedin'),
-    config = models.config.middleware();
+    config = models.config.middleware(),
+    menu = models.navigation.menu();
 
-app.post('/thank-you', config, function (req, res) {
+
+app.post('/thank-you', config, menu, function (req, res) {
     var config = res.locals.config;
     var o = req.body,
         template = '<ul>',
         send = false;
 
-    Object.each(o, function(key, value){
+    Object.each(o, function (key, value) {
         o[key] = value.stripTags().trim();
 
-        if(o[key].length) {
-            template += '<li><label for="">'+ key +': </label>{'+ key +'}</li>';
-            send = true
+        if (o[key].length) {
+            template += '<li><label for="">' + key + ': </label>{' + key + '}</li>';
+            send = true;
         }
     });
 
     template += '</ul>';
 
-    if(send){
+    if (send) {
         o.req = {headers: req.headers, session: req.session, ip: req.ip};
         o.date = new Date();
 
         var message = {};
-        message.to = config.contact.email;
-        message.from = o.email || config.contact.email;
+        message.to = [config.contact.email, o.email]
+        message.from = config.contact.email;
         message.subject = config.contact.subject;
 
         dust.loadSource(dust.compile(template, "email_template"));
 
-        dust.render('email_template', o, function(err, text){
+        dust.render('email_template', o, function (err, text) {
             message.html = text;
         });
 
         mail.sendMail(message, function (err, response) {
-            res.json({ success: !err });
-        })
-    }else{
-        res.json({ success: true });
+            var ctx = { page: (err) ? { title: err.message, content: err.stack } : {title: "תודה!", content: "ההודעה נשלחה בהצלחה." } };
+            res.render('404', ctx);
+        });
+    } else {
+        res.render('index');
     }
 });
